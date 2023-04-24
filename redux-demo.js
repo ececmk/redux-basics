6da1eb4d79936a14f425cc6af29e06b5faeaf7e8
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-04-24 11:12:26 +0200

    increment & decrement added

--- redux-demo.js
+++ redux-demo.js
@@ -1,17 +1,31 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter : 0 }, action) => {
-    return {
-        counter: state.counter + 1
-    };
+
+    if(action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        };
+    }
+
+    if(action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+
+    return state;
+    
 };
 
 const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () => {
    const latestState = store.getState();
+   console.log(latestState)
 };
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({type: 'increment'})
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
